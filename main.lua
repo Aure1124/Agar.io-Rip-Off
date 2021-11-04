aa5c4a5f74b59c450d73f6a45f2236bc65f78f2a
Aure1124 <93620356+Aure1124@users.noreply.github.com>
2021-11-04 17:26:02 -0400

    grapple

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 local x = 0
 local y = 0
 
+local gx, gy = x, y
+local cx, cy = x, y
+
 local fullscreen = false
 local width, height = love.graphics.getDimensions()
 local fwidth, fheight = (1440 - 50), (900 - 50)
@@ -21,14 +24,16 @@ function love.load()
             }
         }
 
-        local invalid = false
-
-        for i,v in ipairs(nomnoms) do
-            invalid = invalid or ((nom.x - v.x)^2 + (nom.y - v.y)^2 < (nom.r + v.r + 8)^2) or (nom.x^2 + nom.y^2 < (15 + 8)^2)
-        end
+        local invalid = (nom.x - x)^2 + (nom.y - y)^2 < (15 + nom.r + 8)^2
 
         if not invalid then
-            table.insert(nomnoms, nom)
+            for i,v in ipairs(nomnoms) do
+                invalid = invalid or ((nom.x - v.x)^2 + (nom.y - v.y)^2 < (nom.r + v.r + 8)^2)
+            end
+
+            if not invalid then
+                table.insert(nomnoms, nom)
+            end
         end
     end
 end
@@ -46,8 +51,35 @@ function love.keypressed(k)
     end
 end
 
+function love.mousepressed(mx,my, k)
+    if k == 1 and math.sqrt((mx - width/2 - x)^2 + (my - height/2 - y)^2) < 350
+    and math.abs(gx - cx) < 5 and math.abs(gy - cy) < 5 then
+        cx = x
+        cy = y
+
+        gx = mx - width/2
+        gy = my - height/2
+    end
+end
+
+function love.update(dt)
+    if gx and gy then
+        if math.abs(gx - cx) > 1 then
+            cx = cx + (gx - cx)*dt*5
+        end
+        
+        if math.abs(gy - cy) > 1 then
+            cy = cy + (gy - cy)*dt*5
+        end
+    end
+end
+
 function love.draw()
     love.graphics.translate(width/2, height/2)
+    
+    if gx and gy then
+        love.graphics.line(x,y, cx,cy)
+    end
     
     for i,v in ipairs(nomnoms) do
         love.graphics.setColor(v.c)
@@ -56,4 +88,5 @@ function love.draw()
 
     love.graphics.setColor(1,1,0)
     love.graphics.circle("fill", x,y, 15)
+
 end
